518a097e47f207c8685874d8002261e9e943853f
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-02-18 14:18:48 +0200

    feat: add api key and generate button function

--- website/app.js
+++ website/app.js
@@ -1,4 +1,13 @@
 /* Global Variables */
+const API_KEY = 'c252888091ee2a2509e0d60013c2883d';
+// const URL = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+const generateButton = document.querySelector('#generate');
+const zipElement = document.querySelector('#zip');
+
+generateButton.addEventListener('click', () => {
+  const zipCode = zipElement.value;
+  console.log(zipCode);
+});
 
 // Create a new date instance dynamically with JS
 let d = new Date();
