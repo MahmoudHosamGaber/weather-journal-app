992669ca533d11646f1bf4441d262c6dc8b68375
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-02-18 15:19:33 +0200

    feat: Add data to the html element

--- website/app.js
+++ website/app.js
@@ -1,14 +1,28 @@
 /* Global Variables */
 const API_KEY = 'c252888091ee2a2509e0d60013c2883d';
-// const URL = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+const URL =
+  'https://api.openweathermap.org/data/2.5/weather?q=new york&APPID=c252888091ee2a2509e0d60013c2883d';
 const generateButton = document.querySelector('#generate');
 const zipElement = document.querySelector('#zip');
+const userFeeling = document.querySelector('#feelings');
+const dateElement = document.querySelector('#date');
+const tempElement = document.querySelector('#temp');
+const contentElement = document.querySelector('#content');
 
 generateButton.addEventListener('click', () => {
   const zipCode = zipElement.value;
-  console.log(zipCode);
-});
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+  // Create a new date instance dynamically with JS
+  const date = new Date();
+  const currentDate =
+    date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
+  const getData = fetch(URL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      dateElement.innerHTML = 'Date: ' + currentDate;
+      tempElement.innerHTML = 'Temperature: ' + data.main.temp;
+      contentElement.innerHTML = userFeeling.value;
+    });
+});
