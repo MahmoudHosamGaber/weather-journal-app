c3cff00957335e2e168a0708e5d826df71d987e3
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-02-20 15:41:13 +0200

    feat: Create postData function to write data to the backend

--- website/app.js
+++ website/app.js
@@ -19,3 +19,23 @@ const getData = async (baseURL, country, key) => {
     console.log('error' + err);
   }
 };
+
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
