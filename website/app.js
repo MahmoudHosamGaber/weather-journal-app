d823564142bde492d8c229af6d9633d5968cf018
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-02-20 15:40:21 +0200

    feat: Create getData function to fetch data from the api

--- website/app.js
+++ website/app.js
@@ -1,7 +1,7 @@
 /* Global Variables */
-const API_KEY = 'c252888091ee2a2509e0d60013c2883d';
-const URL =
-  'https://api.openweathermap.org/data/2.5/weather?q=new york&APPID=c252888091ee2a2509e0d60013c2883d';
+const URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&APPID=c252888091ee2a2509e0d60013c2883d&units=imperial';
+
 const generateButton = document.querySelector('#generate');
 const zipElement = document.querySelector('#zip');
 const userFeeling = document.querySelector('#feelings');
@@ -9,20 +9,13 @@ const dateElement = document.querySelector('#date');
 const tempElement = document.querySelector('#temp');
 const contentElement = document.querySelector('#content');
 
-generateButton.addEventListener('click', () => {
-  const zipCode = zipElement.value;
-
-  // Create a new date instance dynamically with JS
-  const date = new Date();
-  const currentDate =
-    date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
-  const getData = fetch(URL)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      dateElement.innerHTML = 'Date: ' + currentDate;
-      tempElement.innerHTML = 'Temperature: ' + data.main.temp;
-      contentElement.innerHTML = userFeeling.value;
-    });
-});
+const getData = async (baseURL, country, key) => {
+  const res = await fetch(baseURL + country + key);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log('error' + err);
+  }
+};
